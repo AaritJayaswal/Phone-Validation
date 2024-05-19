e136a2eaad23f977e8da1260e8e2394be7657bbe
league <57720048+richardson-liou@users.noreply.github.com>
2024-05-19 10:28:57 -0700

    initial

---
+++ src/main/java/com/example/Phone/Validation/PhoneValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Phone.Validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneValidationApplication.class, args);
+	}
+
+}
